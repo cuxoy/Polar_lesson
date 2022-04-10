b9664cc5bf579e2f8aababad8c992e15c225925f
Dmytro <cuxoy@i.ua>
2022-04-10 15:00:10 +0300

    validated

--- src/js/script.js
+++ src/js/script.js
@@ -19,12 +19,67 @@
             $(this).on('click', function(e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').toggleClass('catalog-item__list_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
             })
             
-          })
+        })
+
+        $('#consultation-form').validate({
+          rules: {
+            name: "required",
+            tel: "required",
+            email: {
+              required: true,
+              email: true
+            }
+          },
+          messages: {
+            name: "введите ваше имя",
+            tel: "введите ваш телефон",
+            email: {
+              required: "ввведите ваш E-mail",
+              email: "неправильный формат"
+            }
+          }
+        });
+        $('#modal-form').validate({
+          rules: {
+            name: "required",
+            tel: "required",
+            email: {
+              required: true,
+              email: true
+            }
+          },
+          messages: {
+            name: "введите ваше имя",
+            tel: "введите ваш телефон",
+            email: {
+              required: "ввведите ваш E-mail",
+              email: "неправильный формат"
+            }
+          }
+        });
+        $('#order-form').validate({
+          rules: {
+            name: "required",
+            tel: "required",
+            email: {
+              required: true,
+              email: true
+            }
+          },
+          messages: {
+            name: "введите ваше имя",
+            tel: "введите ваш телефон",
+            email: {
+              required: "ввведите ваш E-mail",
+              email: "неправильный формат"
+            }
+          }
+        });
       
-    });
+});
     
     //modal---------------------------------------------------------
     $("[data-modal=consultation]").on('click', function(){
@@ -36,14 +91,27 @@
     $('.button_catalog-item').on('click', function(){
         $('.overlay, #order').fadeIn();
     });
-    
-    $('[data-modal="thanks"]').on('click', function(){
-        $('#thanks').fadeIn(), $('#consultation, #order,').fadeOut();
+
+    $('form').submit(function(e) {
+      e.preventDefault();
+      if (!$(this).valid()){
+        return;
+      }
+      $.ajax({
+        type: "Post",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function(){
+        $(this).find("input").val("");
+
+
+        $("form").trigger('reset');
+      });
+      return false;
     });
     // $('[data-modal="thanks"]').on('click', function(){
-    //     $('#consultation, #order,').fadeOut();
+    //     $('#thanks').fadeIn(), $('#consultation, #order,').fadeOut();
     // });
-    
 
 })(jQuery);
 //slider---------------------------------------------------------------
@@ -62,5 +130,18 @@ var slider = tns({
   document.querySelector('.prev').onclick = function () {
     slider.goTo('prev');
   };
+  document.addEventListener('DOMContentLoaded', () => {
+
+    const elements = document.querySelectorAll('[data-mask="phone"]') // ищем все поля с атрибутом data-mask="phone"
+    if (!elements) return // если таких нет, прекращаем выполнение функции
+    const phoneOptions = { // создаем объект параметров
+      mask: '+{38}(000)000-00-00' // задаем единственный параметр mask
+    }
+    elements.forEach(el => { // для каждого найденного поля с атрибутом [data-mask="phone"]
+      IMask(el, phoneOptions) // инициализируем плагин с установленными выше параметрами
+    })
+  
+  })
+  
 
 
